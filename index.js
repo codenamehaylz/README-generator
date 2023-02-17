1c7ea98b50bd1901a86c4b7dd070372acac7f9b0
Hayley <86895234+codenamehaylz@users.noreply.github.com>
2023-02-17 21:35:52 +0000

    changed question array to array of question objects

--- index.js
+++ index.js
@@ -5,15 +5,22 @@ const generateMarkdown = require("./utils/generateMarkdown");
 
 // array of questions for user
 const questions = [
-    "What is the project title?",
-    "Write a description of your project",
-    "How can I install your project?",
-    "How do I use your project?",
-    "Which license would you like to use?",
-    "What are your guidelines for contributing to this project?",
-    "What are the test instructions?",
-    "What is your GitHub username?",
-    "What is your email address?"
+    {type: "input", message: "What is the project title?", name: "title"},
+    {type: "input", message: "Write a description of your project", name: "description"},
+    {type: "input", message: "How can I install your project?", name: "install"},
+    {type: "input", message: "How do I use your project?", name: "usage"},
+    {type: "list", message: "Which license would you like to use?", name: "license", 
+    choices: ["Apache License 2.0", "GNU General Public License v3.0",
+    "MIT License", "BSD 2-Clause 'Simplified' License",
+    "BSD 3-Clause 'New' or 'Revised' License", "Boost Software License 1.0",
+    "Creative Commons Zero v1.0 Universal", "Eclipse Public License 2.0",
+    "GNU Affero General Public License v3.0", "GNU General Public License v2.0",
+    "GNU Lesser General Public License v2.1", "Mozilla Public License 2.0",
+    "The Unlicense"]},
+    {type: "input", message: "What are your guidelines for contributing to this project?", name: "contribute"},
+    {type: "input", message: "What are the test instructions?", name: "testing"},
+    {type: "input", message: "What is your GitHub username?", name: "github"},
+    {type: "input", message: "What is your email address?", name: "email"}
 ];
 
 // function to write README file
@@ -22,7 +29,7 @@ function writeToFile(fileName, data) {
 
 // function to initialize program
 function init() {
-    inquirer
+
 }
 
 // function call to initialize program
