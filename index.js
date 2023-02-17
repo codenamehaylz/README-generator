71a35049dac6f1accfe42099802fe9dd92ca1ffd
Hayley <86895234+codenamehaylz@users.noreply.github.com>
2023-02-17 20:51:41 +0000

    Wrote questions to ask user

--- index.js
+++ index.js
@@ -5,7 +5,15 @@ const generateMarkdown = require("./utils/generateMarkdown");
 
 // array of questions for user
 const questions = [
-
+    "What is the project title?",
+    "Write a description of your project",
+    "How can I install your project?",
+    "How do I use your project?",
+    "Which license would you like to use?",
+    "What are your guidelines for contributing to this project?",
+    "What are the test instructions?",
+    "What is your GitHub username?",
+    "What is your email address?"
 ];
 
 // function to write README file
