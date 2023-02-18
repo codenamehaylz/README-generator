9e0e6d856a895445782888f626854fdd3e08ab3c
Hayley <86895234+codenamehaylz@users.noreply.github.com>
2023-02-18 11:31:41 +0000

    coded the basic layout of the generated README

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,7 +1,28 @@
 // function to generate markdown for README
 function generateMarkdown(data) {
   return `# ${data.title}
+## Description
+${data.description}
+## Table of Contents
 
+## Installation
+${data.install}
+
+## Usage
+${data.usage}
+
+## License
+${data.license}
+
+## Contributing
+${data.contribute}
+
+## Tests
+${data.testing}
+
+## Questions
+${data.github}
+${data.email}
 `;
 }
 
