097a9971ca45fdca1ce62eb51f72059d9c3d218f
Hayley <86895234+codenamehaylz@users.noreply.github.com>
2023-02-19 11:58:12 +0000

    table of contents now links to each section

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -3,7 +3,20 @@ function generateMarkdown(data) {
   return `# ${data.title}
 ## Description
 ${data.description}
+
 ## Table of Contents
+* [Installation](#installation)
+
+* [Usage](#usage)
+
+* [License](#license)
+
+* [Contributing](#contributing)
+
+* [Tests](#tests)
+
+* [Questions](#questions)
+
 
 ## Installation
 ${data.install}
@@ -22,6 +35,7 @@ ${data.testing}
 
 ## Questions
 ${data.github}
+
 ${data.email}
 `;
 }
